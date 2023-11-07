36403bcda50b040d1ecf841d73153df7e91b42e6
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-07 16:54:35 +0200

    a

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,24 @@
+import { useMemo, useState } from 'react';
+
 import { Statistics } from '../Statistics/Statistics';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Section } from '../Section/Section';
 import { Notification } from '../Notification/Notification';
 
 import { Layout } from 'components/Layout';
-import { useState } from 'react';
 
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const countTotalFeedback = () => {
+  const countTotalFeedback = useMemo(() => {
     return good + neutral + bad;
-  };
+  }, [good, neutral, bad]);
 
-  const countPositiveFeedbackPercentage = () => {
-    return Math.round((good / countTotalFeedback()) * 100 || 0);
-  };
+  const countPositiveFeedbackPercentage = useMemo(() => {
+    return Math.round((good / countTotalFeedback) * 100 || 0);
+  }, [good, countTotalFeedback]);
 
   const leaveFeedback = option => {
     switch (option) {
@@ -41,13 +42,13 @@ export const App = () => {
         />
       </Section>
       <Section title="Statistics">
-        {countTotalFeedback() > 0 ? (
+        {countTotalFeedback > 0 ? (
           <Statistics
             good={good}
             neutral={neutral}
             bad={bad}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercentage()}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
         ) : (
           <Notification message="There is no feedback" />
